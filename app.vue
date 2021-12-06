882b1b414ccfca72f7795e9fc8485d2ec46e7cd5
Nate King <codewise.nate@gmail.com>
2021-12-05 19:36:36 -0800

    initial content

--- app.vue
+++ app.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>jessecahill.com</h1>>
   </div>
 </template>
+<script></script>
+<style>
+body {
+  display: flex;
+  justify-content: center;
+}
+h1 {
+  color: #fff;
+  margin: 4rem auto;
+}
+</style>
